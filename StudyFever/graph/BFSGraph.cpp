a7227573b242341de33e17aa5163767405f1e021
Sourav <souravgope765@gmail.com>
2024-06-18 20:17:04 +0530

    studyFever: ADD: BFS operation and AdjacencyList of Graph

--- StudyFever/graph/BFSGraph.cpp
+++ StudyFever/graph/BFSGraph.cpp
@@ -1,5 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 void display(vector<int> arr[], int size) {
@@ -17,6 +18,25 @@ void addEdge(vector<int> arr[], int u, int v) {
     arr[v].push_back(u);
 }
 
+void BFS(vector<int> arr[], int size, int start){
+vector <bool> visited(size,false);
+queue <int> q;
+q.push(start);
+visited[start] = true;
+while(!q.empty()){
+    int u = q.front();
+    cout << u << endl;
+    q.pop();
+    for(int x : arr[u]){
+        if(visited[x] == false){
+            visited[x]= true;
+            q.push(x);
+        }
+    }
+}
+
+}
+
 int main() {
     int _size_ = 4;
     vector<int> arr[_size_];
@@ -25,8 +45,8 @@ int main() {
     addEdge(arr, 0, 2);
     addEdge(arr, 1, 2);
     addEdge(arr, 1, 3);
-
     display(arr, _size_);
+    BFS(arr,_size_,0);
 
     return 0;
 }
